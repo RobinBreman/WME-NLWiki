5cbc0658780ec549a2f28515e5ec2149ef9b48d5
Robin Breman <rbreman@gmail.com>
2025-04-29 10:30:33 +0200

    v1

---
+++ README.md
@@ -0,0 +1,16 @@
+# WME-NL-Wiki
+
+This script will change the wme 'Krijg hulp' link from Discuss https://wazeopedia.waze.com into http://wazeopedia.nl
+
+## Code
+Feel free to improve this script I will be happy to accept any pull-requests.
+https://github.com/RobinBreman/WME-NL-Wiki
+
+## Changelog
+|Version||
+1.0.0 | Initial version
+
+
+
+## About me
+This script is developed by Robin Breman (@robbre), L4 editor Waze NL.
---
+++ wme-nlwiki.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name 			WME-NLWiki
+// @description 	Adds links to other maps
+// @namespace 		http://tampermonkey.net/
+// @author          Robin Breman | Waze NL | @robbre | https://github.com/RobinBreman/WME-MapLinks
+// @match           *://*.waze.com/*editor*
+// @exclude         *://*.waze.com/user/editor*
+// @grant 			none
+// @version 		1.0.0
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const version = '1.0.0';
+    
+    function wmescript_bootstrap() {
+        const wazeapi = window.W;
+        if (!wazeapi || !wazeapi.map) {
+            setTimeout(wmescript_bootstrap, 1000);
+            return;
+        }
+        document.querySelector("#OpenLayers_Control_133 > a:nth-child(5)").href='http://wazeopedia.nl'; 
+    }
+
+    setTimeout(wmescript_bootstrap, 5000);
+})();
